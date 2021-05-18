646c37537d1eea831271e9c8ed0d84c0f43d55ad
ManosAnast <manosanastasaki@gmail.com>
2021-05-18 16:45:08 +0300

    script done

--- create_infiles.sh
+++ create_infiles.sh
@@ -13,87 +13,81 @@ inputfile="./$1"  #number of lines that the file will have
 input_dir=$2 #duplicateallowed flag
 numfiles=$3
 
-# if [[ -d "$input_dir" ]] #Check if directory exists. If it doesn't, create it
-#     then
-#     echo Directory already exist
-#     exit
-# else
-#     mkdir -p "./$input_dir"
-# fi
+if [[ -d "$input_dir" ]] #Check if directory exists. If it doesn't, create it
+    then
+    echo Directory already exist
+    exit
+else
+    mkdir -p "./$input_dir"
+fi
 
 # Give the input file to an array.
 # Each array "object" is one word. Therefore, we can calculate where the countries are.
-mapfile -d " " array < "$inputfile"
-arrayMax=${#array[@]}
+mapfile -t arrayLine < "$inputfile"
+IFS=' ' read -r -a arrayData <<< ${arrayLine[@]}
+arrayMax=${#arrayData[@]}
 
-while (($i<$arrayMax))
+# Create subdirectories and files for every country
+let i=3
+let k=0
+while [[ $i -le $arrayMax ]]
     do
-    echo while
-    i=$arrayMax
+
+    if [[ ! -d "./$input_dir${arrayData[$i]}" ]] # If the subdirectory doesn't exist, create it and it's files.
+        then
+        mkdir -p "./$input_dir${arrayData[$i]}"
+        for ((j=1; j<=$numfiles; j++))
+            do
+            touch "./$input_dir${arrayData[$i]}/${arrayData[$i]}-$j.txt"
+        done
+    fi
+    
+    # See readme.
+    i=$(($i+3))
+    if [[ ("${arrayData[$i]}" == "YES") ]]
+        then
+        i=$(($i+5))
+    else
+        i=$(($i+4))
+    fi
+    k=$(($k+1))
+done
+
+# Traverse the subdirectories that you made at the previous loop.
+# Based on the country of every entry put it at the correct subdirectory.
+dirlist=$(find $"./$input_dir" -mindepth 1 -maxdepth 1 -type d) # List of the subdirectories
+for dir in $dirlist
+do
+    cd $dir # Enter the subdirectory
+    files=$(find -not -name "*1.txt" -type f) # Find all the files that are not the first one
+    firstfile=$(find -name "*1.txt" -type f) # Find the first file
+    IFS=' ' read -r -a filelist <<< ${files[@]} # Make an array with the files
+
+    let i=3
+    let k=0
+    let j=1
+    while [[ $i -lt $arrayMax ]] # The same loop as before, but now we insert to the appropriate file the entries
+    do
+        if [[ ("./$input_dir${arrayData[$i]}" == "$dir") ]]
+        then
+            if [[ $j == 1 ]] || [[ $numfiles == 1 ]]
+            then
+                echo ${arrayLine[$k]} >> "${firstfile}"
+                j=0
+            else
+                echo ${arrayLine[$k]} >> "${filelist[$j]}"
+                j=$(($j+1))
+            fi
+        fi
+
+        i=$(($i+3))
+        if [[ ("${arrayData[$i]}" == "YES") ]]
+            then
+            i=$(($i+5))
+        else
+            i=$(($i+4))
+        fi
+        k=$(($k+1))
     done
-# if [[ ( $lines > 10000) && ($duplicate == 0) ]] # Check if numLines are within the limit for the ids.
-#     then
-#     echo With duplication disabled, there can be 10000 Ids. If you want to proceed duplication must be enabled.
-#     read -p "If you want to proceed press yes: " yes
-#     if [[ ("$yes"=="yes") ]] # If user seems to want to continue, enable duplication and make idNum=9999.
-#         then
-#         idNum=9999
-#         duplicate=1
-#     else
-#         exit
-#     fi
-# elif [[ ( $lines == 10000) && ($duplicate == 0) ]]
-#     then
-#     idNum=9999
-#     lines=9999
-# else
-#     idNum=$[$lines]
-# fi
-
-# # Name of file that the script is writing to
-# input=inputFile.txt
-
-# virus=($(cat "$1")) # Array with viruses names
-# countries=($(cat "$2")) # Array with countries
-# viruslength=$[${#virus[@]}] # Length of virus array
-# countrieslength=$[${#countries[@]}] # Length of countries array
-
-# id=($(shuf -i 0-$idNum -n $lines)) # Array with rundom id's that are going to be used.
-
-# # Empty the new file from the previous things that it contained and insert the first entry
-# if [ $(( $RANDOM % 2 )) == 1 ]
-#     then
-#     vac="YES"
-#     Date="$(( $RANDOM % 30 + 1 ))-$(( $RANDOM % 12 +1 ))-$(( $RANDOM % 50 + 2000 ))"
-# else
-#     vac="NO"
-#     Date=""
-# fi 
-# var="${id[0]} name${id[0]} surname${id[0]} ${countries[$(( $RANDOM % $countrieslength ))]} $(( $RANDOM % 120 + 1 )) ${virus[$(( $RANDOM % $viruslength ))]} $vac $Date"
-# echo $var > $input
-
-# # Make of input file
-# flag=0
-# for ((i=1; i<$lines; i++))
-# do
-#     if [[ ( $duplicate != 0 ) && ( $(( $RANDOM % 2 )) == 1 ) && ( $flag == 0 ) ]] # Checks if duplication is allowed
-#         then 
-#         flag=1
-#         echo $var >> $input
-#         continue
-#     fi
-
-#     if [ $(( $RANDOM % 2 )) == 1 ] # Figures out if the citizen is vaccinated or not
-#         then
-#         vac="YES"
-#         Date="$(( $RANDOM % 30 + 1 ))-$(( $RANDOM % 12 +1 ))-$(( $RANDOM % 50 + 2000 ))"
-#     else
-#         vac="NO"
-#         Date=""
-#     fi 
-
-#     flag=0
-#     var="${id[$i]} name${id[$i]} surname${id[$i]} ${countries[$(( $RANDOM % $countrieslength ))]} $(( $RANDOM % 120 + 1 )) ${virus[$(( $RANDOM % $viruslength ))]} $vac $Date"
-#     echo $var >> $input
-
-# done
+    cd ../..
+done
